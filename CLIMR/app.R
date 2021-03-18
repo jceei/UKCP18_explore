2e13e4d2a16539dceb052b92ccc9bc07796dfca8
Fiona Spooner <spoonerfiona@gmail.com>
2021-03-18 15:23:56 +0000

    updates to app and data used within it

--- CLIMR/app.R
+++ CLIMR/app.R
@@ -1,84 +1,121 @@
-#
-# This is a Shiny web application. You can run the application by clicking
-# the 'Run App' button above.
-#
-# Find out more about building applications with Shiny here:
-#
-#    http://shiny.rstudio.com/
-#
-
 library(shiny)
 library(leaflet)
 library(sf)
 library(dplyr)
+library(thematic)
+library(bslib)
 #load("climr_bris_data_84.RDA")
-bristol_data<- readRDS("dummy_data_bristol.RDS")
+bristol_data<- readRDS("dummy_data_bristol.RDS") %>% 
+    mutate(hazard = hazard_sc)
+years <- unique(bristol_data$year)
+risk_empty <- unique(bristol_data %>% select(msoa11cd))
 
-exposure <- bristol_data %>% 
-    select(msoa11cd, exposure) 
-
-vulnerability <- bristol_data %>% 
-    select(msoa11cd, vulnerability) 
+vulnerability <- unique(bristol_data %>% 
+                            dplyr::select(msoa11cd, vulnerability))
 
 hazard <- bristol_data %>% 
-    select(msoa11cd, hazard) 
+    dplyr::select(msoa11cd, hazard = hazard_sc, year) 
 
-risk <- bristol_data %>% 
-        select(msoa11cd, risk) 
+# risk <- bristol_data %>%
+#     dplyr::select(msoa11cd, risk, year)
 
 
-pal_exposure <- colorBin("RdPu", domain = as.numeric(exposure$exposure), na.color = "transparent")
-pal_vulnerability <- colorBin("Purples", domain = as.numeric(vulnerability$vulnerability), na.color = "transparent")
-pal_hazard <- colorBin("Reds", domain = as.numeric(hazard$hazard), na.color = "transparent")
-pal_risk <- colorBin("YlOrRd", domain = as.numeric(risk$risk), na.color = "transparent")
-
-# Define UI for application that draws a histogram
+thematic::thematic_shiny()
 ui <- fluidPage(
     
     theme = bslib::bs_theme(version = 4, bootswatch = "minty"),
-
-    # Application title
-    titlePanel("CLIMR example"),
-
-        # Show a plot of the generated distribution
-        mainPanel(
-            leafletOutput("climrPlot", height=600, width = 900)
-        )
+    
+    tags$head(tags$link(rel="shortcut icon", href="JCEEI_logo.png")),
+    
+    navbarPage(
+        "Bristol Climate Heat Risk",
+        tabPanel("Map",
+    titlePanel(title = "",windowTitle = "Urban Heat"),
+    # Show a plot of the generated distribution,
+    sidebarLayout(position = "right",
+                  sidebarPanel(
+                      selectInput("year",
+                                  "Hazard - select year:",
+                                  years,
+                                  selected = "2076"),
+                    sliderInput("exposure", "Exposure - select % population exposed:",
+                              min = 0, max = 100, value = 100
+                  ), width = 3),
+                  # Application title
+                  mainPanel(
+                      leafletOutput("climrPlot", height=600, width = 900),
+                      img(src = "Final_logo_with_space.png",width="174.4",height="65.2", align = "right"),
+                      
+                  )
     )
-
+        ),
+    tabPanel("Data",
+             sidebarLayout(
+                 
+                 # Sidebar panel for inputs ----
+                 sidebarPanel(
+                     
+                     # Button
+                     downloadButton("downloadData", "Download")
+                     
+                 ),
+                 
+                 # Main panel for displaying outputs ----
+                 mainPanel(
+                     
+                     tableOutput("outputTable")
+                     
+                 )
+             )
+    )
+)
+)
 # Define server logic required to draw a histogram
 server <- function(input, output) {
-
+    
+    
     output$climrPlot <- renderLeaflet({
         
-    labels <- sprintf(
-        "<strong>%s</strong><br/>Exposure: %g <br/>Vulnerability: %g <br/>Hazard: %g <br/>Risk: %g" ,
-        bristol_data$msoa11nm, bristol_data$exposure,bristol_data$vulnerability,
-        bristol_data$hazard, bristol_data$risk
-    ) %>% lapply(htmltools::HTML)
+        bristol_data_fil <- bristol_data[bristol_data$year == input$year,]
+        hazard_fil <- hazard[hazard$year == input$year,]
+        bristol_data_fil$exposure <- input$exposure/100
+        risk_empty$risk <- hazard_fil$hazard * vulnerability$vulnerability * input$exposure/100
         
-       leaflet(bristol_data) %>%
+           
+        pal_exposure <- colorBin("RdPu", domain = c(0,1), na.color = "transparent")
+        pal_vulnerability <- colorBin("Purples", domain = c(min(bristol_data$vulnerability),max(bristol_data$vulnerability)), na.color = "transparent")
+        pal_hazard <- colorBin("Reds", domain = c(min(bristol_data$hazard),max(bristol_data$hazard)), na.color = "transparent")
+        pal_risk <- colorBin("YlOrRd", domain = c(0,1), na.color = "transparent")
+        
+        
+        labels <- sprintf(
+            "<strong>%s</strong><br/>Exposure: %g <br/>Vulnerability: %g <br/>Hazard: %g <br/>Risk: %g" ,
+            bristol_data_fil$msoa11nm, bristol_data_fil$exposure,bristol_data_fil$vulnerability,
+            bristol_data_fil$hazard, risk_empty$risk
+        ) %>% lapply(htmltools::HTML)
+        
+        leaflet(bristol_data_fil) %>%
             setView(-2.6, 51.47, 11) %>%
             addTiles() %>%
-                      addPolygons(data = exposure,
-                        fillColor = pal_exposure(exposure$exposure),
-                group = "Exposure",
-                weight = 2,
-                opacity = 1,
-                color = "white",
-                dashArray = "3",
-                fillOpacity = 0.8,
-                highlight = highlightOptions(
-                    weight = 5,
-                    color = "#666",
-                    dashArray = "",
-                    fillOpacity = 0.5,
-                    bringToFront = TRUE),
-                label = labels,
-                labelOptions = labelOptions(
-                    style = list("font-weight" = "normal", padding = "3px 8px"),
-                    textsize = "15px",
-                    direction = "auto")) %>% 
+            addPolygons(data = bristol_data_fil,
+                        fillColor = pal_exposure(bristol_data_fil$exposure),
+                        group = "Exposure",
+                        weight = 2,
+                        opacity = 1,
+                        color = "white",
+                        dashArray = "3",
+                        fillOpacity = 0.8,
+                        highlight = highlightOptions(
+                            weight = 5,
+                            color = "#666",
+                            dashArray = "",
+                            fillOpacity = 0.5,
+                            bringToFront = TRUE),
+                        label = labels,
+                        labelOptions = labelOptions(
+                            style = list("font-weight" = "normal", padding = "3px 8px"),
+                            textsize = "15px",
+                            direction = "auto")) %>% 
             addPolygons(data = vulnerability,
                         fillColor = pal_vulnerability(vulnerability$vulnerability),
                         group = "Vulnerability",
@@ -98,8 +135,8 @@ server <- function(input, output) {
                             style = list("font-weight" = "normal", padding = "3px 8px"),
                             textsize = "15px",
                             direction = "auto")) %>% 
-            addPolygons(data = hazard,
-                        fillColor = pal_hazard(hazard$hazard),
+            addPolygons(data = hazard_fil,
+                        fillColor = pal_hazard(hazard_fil$hazard),
                         group = "Hazard",
                         weight = 2,
                         opacity = 1,
@@ -117,8 +154,8 @@ server <- function(input, output) {
                             style = list("font-weight" = "normal", padding = "3px 8px"),
                             textsize = "15px",
                             direction = "auto")) %>% 
-            addPolygons(data = risk,
-                        fillColor = pal_risk(risk$risk),
+            addPolygons(data = risk_empty,
+                        fillColor = pal_risk(risk_empty$risk),
                         group = "Risk",
                         weight = 2,
                         opacity = 1,
@@ -136,12 +173,33 @@ server <- function(input, output) {
                             style = list("font-weight" = "normal", padding = "3px 8px"),
                             textsize = "15px",
                             direction = "auto")) %>% 
-       addLayersControl(
-           overlayGroups = c("Exposure", "Vulnerability", "Hazard", "Risk"),
-           options = layersControlOptions(collapsed = FALSE, autoZIndex = TRUE)
-       )
+            addLayersControl(
+                overlayGroups = c("Exposure", "Vulnerability", "Hazard", "Risk"),
+                options = layersControlOptions(collapsed = FALSE, autoZIndex = TRUE)
+            )
         
     })
+    
+    outputTable <- renderTable({
+        
+     out_data <-  data.frame(msoa11cd = bristol_data_fil$msoa11cd, 
+                               msoa11nm = bristol_data_fil$msoa11nm,
+                               year = bristol_data_fil$year,
+                               exposure = input$exposure/100,
+                               vulnerability = vulnerability$vulnerability,
+                               hazard =  hazard_fil$hazard,
+                               risk = risk_empty$risk) %>% 
+            arrange(msoa11nm )
+     
+     out_data
+    })
+    
+    output$downloadData <- downloadHandler(
+        filename = paste("bristol_",input$year, "_exposure_", input$exposure , ".csv", sep = ""),
+        content = function(file) {
+            write.csv(output$outputTable, file, row.names = FALSE)
+        }
+    )
 }
 
 # Run the application 
